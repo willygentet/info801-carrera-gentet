b27b0689866ed968319440ffe99c6d491cf63dc1
vastu <vatsuna74@gmail.com>
2022-04-07 09:37:13 +0200

    react

--- maitre_oeuvre/src/App.js
+++ maitre_oeuvre/src/App.js
@@ -18,6 +18,9 @@ class App extends React.Component{
         data: [],
     }
   }
+  componentDidUpdate(){
+    this.showComponent("Home")
+  }
   componentDidMount(){
     axios.defaults.baseURL = 'http://127.0.0.1:5000';
     axios.defaults.headers.post['Content-Type'] ='application/json;charset=utf-8';
--- maitre_oeuvre/src/components/AjouterOffre.js
+++ maitre_oeuvre/src/components/AjouterOffre.js
@@ -1,7 +1,25 @@
 import React from "react";
+import axios from "axios";
 
 class AjouterOffre extends React.Component {
-
+    constructor(props) {
+        super(props);
+        this.state = {
+            requirements:"",
+            cost:0,
+            time:0,
+            quantity:0
+        }
+      }
+    addOffer(){
+        axios.defaults.baseURL = 'http://127.0.0.1:5000';
+        axios.defaults.headers.post['Content-Type'] ='application/json;charset=utf-8';
+        axios.defaults.headers.post['Access-Control-Allow-Origin'] = '*';
+        axios.put("/offers?requirements="+this.state.requirements+"&cost="+this.state.cost+"&time="+this.state.time+"&quantity="+this.state.quantity)
+            .then((res) => {
+                document.getElementById("popup-resolution").classList.remove('is-active')
+            })
+    }
     render() {
         return (
             <div id="popup-resolution" className="modal">
@@ -11,18 +29,18 @@ class AjouterOffre extends React.Component {
                     <h1 className="title has-text-centered">Ajouter une offre</h1>
                     <div id="form" className="has-text-centered">
                         <label className="subtitle" htmlFor="caracteristiques">Caractéristiques technique : </label>
-                        <textarea className="textarea is-info" placeholder="Caractéristiques" name="caracteristiques" id="caracteristiques"></textarea>
+                        <textarea className="textarea is-info" placeholder="Caractéristiques" name="caracteristiques" id="caracteristiques" onChange={event => this.setState({requirements: event.target.value})}></textarea>
                         <label className="subtitle" htmlFor="cout">Cout : </label>
-                        <input className="input is-info" type="number" id="cout" name="cout" placeholder="Cout"></input>
+                        <input className="input is-info" type="number" id="cout" name="cout" placeholder="Cout" onChange={event => this.setState({cost: event.target.value})}></input>
                         <label className="subtitle" htmlFor="temps">Temps : </label>
-                        <input className="input is-info" type="number" id="temps" name="temps" placeholder="Nb jours"></input>
+                        <input className="input is-info" type="number" id="temps" name="temps" placeholder="Nb jours" onChange={event => this.setState({time: event.target.value})}></input>
                         <label className="subtitle" htmlFor="quantite">Quantité : </label>
-                        <input className="input is-info" type="number" id="quantite" name="quantite" placeholder="Volume"></input>
+                        <input className="input is-info" type="number" id="quantite" name="quantite" placeholder="Volume" onChange={event => this.setState({quantity: event.target.value})}></input>
                         <button className="button m-2 is-success" onClick={() => document.getElementById("popup-resolution").classList.remove('is-active')}>Ajouter</button>
                     </div>
                 </div>
             </div>
-            <button className="modal-close is-large" aria-label="close" onClick={() => document.getElementById("popup-resolution").classList.remove('is-active')}></button>
+            <button className="modal-close is-large" aria-label="close" onClick={() =>this.addOffer()}></button>
         </div>
         );
     }
