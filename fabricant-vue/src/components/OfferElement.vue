82c405577a1a462e41f2224ae582d2d2fe4ccc39
willygentet <58994972+willygentet@users.noreply.github.com>
2022-04-11 10:16:34 +0200

    vue aled thomas

--- fabricant-vue/src/components/OfferElement.vue
+++ fabricant-vue/src/components/OfferElement.vue
@@ -15,24 +15,81 @@
       <div class="subtitle has-text-white">Cout : {{ cost }}</div>
       <div class="subtitle has-text-white">Time : {{ time }}</div>
       <div class="subtitle has-text-white">Quantité : {{ quantity }}</div>
+
       <button @click="showModal()" class="button is-success">Proposer</button>
-      <add-proposition-modal
-        :requierements="requierements"
-        :cost="cost"
-        :time="time"
-        :quantity="quantity"
-        :propositions="propositions"
-      />
+
+      <div id="popup-resolution" class="modal">
+        <div class="modal-background"></div>
+        <div class="modal-content">
+          <div class="box">
+            <h1 class="title has-text-centered">Ajouter une offre</h1>
+            <div id="form" class="has-text-centered">
+              <label class="subtitle" htmlFor="caracteristiques"
+                >Caractéristiques technique :
+              </label>
+              <textarea
+                class="textarea is-info"
+                placeholder="Caractéristiques"
+                name="caracteristiques"
+                id="caracteristiques"
+                :value="requierements"
+              ></textarea>
+              <label class="subtitle">Cout : </label>
+              <input
+                class="input is-info"
+                type="number"
+                id="cout"
+                name="cout"
+                placeholder="Cout"
+                value="cost"
+              />
+              <label class="subtitle">Temps : </label>
+              <input
+                class="input is-info"
+                type="number"
+                id="temps"
+                name="temps"
+                placeholder="Nb jours"
+                value="time"
+              />
+              <label class="subtitle">Quantité : </label>
+              <input
+                class="input is-info"
+                type="number"
+                id="quantite"
+                name="quantite"
+                placeholder="Volume"
+                value="quantity"
+              />
+              <button class="button m-2 is-success">Ajouter</button>
+            </div>
+          </div>
+        </div>
+        <button
+          @click="closeModal()"
+          class="modal-close is-large"
+          aria-label="close"
+        ></button>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-import AddPropositionModal from "./AddPropositionModal.vue";
+//import AddPropositionModal from "./AddPropositionModal.vue";
 export default {
-  components: { AddPropositionModal },
+  components: {},
   name: "OfferElement",
-  methods: {},
+  methods: {
+    showModal() {
+      let modal = document.querySelector(".modal");
+      modal.classList.add("is-active");
+    },
+    closeModal() {
+      let modal = document.querySelector(".modal");
+      modal.classList.remove("is-active");
+    },
+  },
   props: {
     requierements: {
       type: String,
